28e48a5b24a7e84034b401d49026ca6de3b50123
kama93 <kamilaszabela@gmail.com>
2021-06-24 08:10:24 +0100

    add last mobile view changes

--- front-end/src/component/world/world.tsx
+++ front-end/src/component/world/world.tsx
@@ -23,7 +23,7 @@ function World() {
 
   return (
     <div>
-      {world &&
+      {world ?
         <div className='world-container'>
           {world.map((info, i) => (
             <div id={`card${i}`} className="card-world" key={i}>
@@ -35,7 +35,17 @@ function World() {
                   <Meta title={info.title} description={info.description} />
                 </Card> </a>
             </div>))}
-        </div>}
+        </div> :
+        <div className="divLoader">
+          <svg className="svgLoader" viewBox="0 0 100 100" width="10em" height="10em">
+            <path ng-attr-d="{{config.pathCmd}}" ng-attr-fill="{{config.color}}" stroke="none"
+              d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" fill="#51CACC" transform="rotate(179.719 50 51)">
+              <animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 51;360 50 51"
+                keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform>
+            </path>
+          </svg>
+        </div>
+      }
     </div>
   )
 }
